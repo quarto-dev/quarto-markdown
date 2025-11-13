e5d13e3f801c97b1bead1618ad0edef90a77ec19
Carlos Scheidegger <carlos.scheidegger@posit.co>
2025-11-13 15:25:57 -0600

    fix

--- crates/quarto-markdown-pandoc/fuzz/fuzz_targets/hello_fuzz.rs
+++ crates/quarto-markdown-pandoc/fuzz/fuzz_targets/hello_fuzz.rs
@@ -10,6 +10,7 @@ use quarto_markdown_pandoc::readers;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = crate::readers::qmd::read(s.as_bytes(), false, "<input>", &mut std::io::sink());
+        let _ =
+            crate::readers::qmd::read(s.as_bytes(), false, "<input>", &mut std::io::sink(), true);
     }
 });
